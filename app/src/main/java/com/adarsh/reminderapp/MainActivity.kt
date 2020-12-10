7c4a19b0a0d812b5c5b813d5032df448be18d8bb
Adarsh Latiyan <adarshlatiyan@gmail.com>
2020-12-10 16:50:09 +0530

    Supports up navigation and fragment labels corrected

--- app/src/main/java/com/adarsh/reminderapp/MainActivity.kt
+++ app/src/main/java/com/adarsh/reminderapp/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.adarsh.reminderapp
 
-import android.app.AlarmManager
-import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    private val alarmManager by lazy { getSystemService(Context.ALARM_SERVICE) as AlarmManager }
+
+    private val navController by lazy {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val pi = PendingIntent.getBroadcast(this, AlarmReceiver.REMINDER_REQUEST_CODE, Intent(this, AlarmReceiver::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
-//        alarmManager.setExact(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pi)
+
+        setupActionBarWithNavController(
+            navController,
+            AppBarConfiguration.Builder(R.id.reminderListFragment).build()
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/adarsh/reminderapp/util/AlarmHelper.kt
+++ app/src/main/java/com/adarsh/reminderapp/util/AlarmHelper.kt
@@ -4,7 +4,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.adarsh.reminderapp.AlarmReceiver
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.util.*
@@ -21,16 +20,23 @@ class AlarmHelper @Inject constructor(@ApplicationContext private val context: C
     private val alarmManager by lazy { context.getSystemService(Context.ALARM_SERVICE) as AlarmManager }
 
     fun setAlarm(calendar: Calendar, pk: Int) {
-        Log.d(TAG, "setAlarm: ${Calendar.getInstance().timeInMillis}")
-        Log.d(TAG, "setAlarm: ${Date(calendar.timeInMillis)}")
-        Log.d(TAG, "setAlarm: ${calendar.timeInMillis}")
-
         val intent = Intent(context, AlarmReceiver::class.java).apply {
             putExtra(EXTRA_REMINDER_PK, pk)
         }
         val pi = PendingIntent.getBroadcast(context, pk, intent, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, pi)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M)
+            alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                calendar.timeInMillis - 1000,
+                pi
+            )
+        else alarmManager.setExact(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis - 1000,
+            pi
+        )
+
 
 //        RESTART ALARM IF DEVICE IS REBOOTED
 
